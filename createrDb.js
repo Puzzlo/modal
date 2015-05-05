398095599dbb6192e1c1f64814374b595df17dd9
Esskov Andrey <p_uzo@mail.ru>
2015-05-05 17:24:05 +0300

    mongodb tests

--- createrDb.js
+++ createrDb.js
@@ -1,14 +1,50 @@
+var mongoose = require('libs/mongoose');
+var async = require('async');
 var User = require('models/user').User;
 
-var user = new User({
-    username: "Tester1",
-    password: "secret"
+async.series([
+    open,
+    dropDataBase,
+    createUsers,
+    close
+], function(err, results){
+    console.log(arguments);
 });
 
-user.save(function (err, user, affected) {
-   if(err) throw err;
 
-    User.findOne({ username: "Tester1"}, function(err, tester){
-        console.log(tester);
-    });
-});
+function open(callback){
+    mongoose.connection.on('open',callback);
+}
+
+function dropDataBase(callback){
+    var db = mongoose.connection.db;
+    db.dropDatabase(callback);
+}
+
+function createUsers(callback){
+    async.parallel([
+        function (callback) {
+            var vasya = new User({username: 'Вася', password: 'super'});
+            vasya.save(function (err) {
+                callback(err, vasya);
+            });
+        },
+        function (callback) {
+            var petya = new User({username: 'Петя', password: 'tornado'});
+            petya.save(function (err) {
+                callback(err, petya);
+            });
+        },
+        function (callback) {
+            var admin = new User({username: 'ADMIN', password: 'admin'});
+            admin.save(function (err) {
+                callback(err, admin);
+            });
+        }
+    ], callback);
+}
+
+function close(callback){
+    mongoose.disconnect(callback);
+}
+
