ce44adae54bc238ee4be8d4d39d586c7509dc07a
Puzzlo <p_uzo@mail.ru>
2015-05-11 21:32:45 +0300

    bdd

---
+++ public/js/registrForm.js
@@ -0,0 +1,106 @@
+// registrForm.js 
+
+$(document).ready(function () {
+   var app = {
+       initialize: function() {
+           this.setUpListeners();
+           console.log('iniatialize');
+       },
+
+       setUpListeners: function() {
+           $('#register').on('submit', app.submitForm);
+           $('#register').on('keyup', 'input', app.removeError);
+       },
+
+       submitForm: function(e) {
+           e.preventDefault();
+
+           var form = $(this),
+               submitBtn = form.find('button[type="submit"]');
+           console.log( 'ser=' + form.serialize() );
+
+           if( app.validateForm(form) === false ) return false;
+           submitBtn.attr('disabled', 'disabled');
+           console.log('go in ajax');
+           $.ajax({
+                url: "/registration",
+                method: "POST",
+                data: {name: form.find('#inputRegister').val(), pass: form.find('#inputPasswordReg').val()},
+               statusCode: {
+                   200: function(id) {
+                       window.location.href = '/users/:'+id;
+                       console.log('registration success');
+                   },
+                   403: function (jqXHR) {
+                       var error = JSON.parse(jqXHR.responseText);
+                       $('.error', form).html(error.message);
+                   }
+               }
+           });
+           $("#register").modal('hide');
+       },
+
+       validateForm: function(form) {
+           var valid = true,
+               login = form.find('#inputRegister'),
+               pass1 = form.find('#inputPasswordReg'),
+               pass2 = form.find('#inputPasswordRegAgain'),
+               loginValue = login.val(),
+               pass1Value = pass1.val(),
+               pass2Value = pass2.val(),
+               formReg = login.parents('.form-group'),
+               formPass1 = pass1.parents('.form-group'),
+               formPass2 = pass2.parents('.form-group');
+           if ( loginValue.length < 1 ) {
+                valid = false;
+               formReg.addClass('has-error').removeClass('has-success');
+               login.tooltip({
+                   trigger: 'manual',
+                   placement: 'right',
+                   title: 'min 5 letter'
+               }).tooltip('show');
+           } else {
+               formReg.addClass('has-success').removeClass('has-error');
+           }
+           if ( pass1Value.length === 0 ) {
+               formPass1.addClass('has-error').removeClass('has-success');
+               valid = false;
+               pass1.tooltip({
+                   trigger: 'manual',
+                   placement: 'right',
+                   title: 'password minimum 1 letter'
+               }).tooltip('show');
+               return false;
+           } else {
+               formPass1.addClass('has-success').removeClass('has-error');
+           }
+           if ( pass1Value !== pass2Value ) {
+               valid = false;
+               formPass1.addClass('has-error').removeClass('has-success');
+               formPass2.addClass('has-error').removeClass('has-success');
+               pass1.tooltip({
+                   trigger: 'manual',
+                   placement: 'right',
+                   title: 'passwords must match'
+               }).tooltip('show');
+               pass2.tooltip({
+                   trigger: 'manual',
+                   placement: 'right',
+                   title: 'passwords must match'
+               }).tooltip('show');
+           } else {
+               formPass1.addClass('has-success').removeClass('has-error');
+               formPass2.addClass('has-success').removeClass('has-error');
+           }
+
+
+           return valid;
+       },
+
+       removeError: function() {
+           $(this).tooltip('destroy').parents('form-group').removeClass('has-error');
+       }
+   };
+
+    app.initialize();
+});
--- routes/registration.js
+++ routes/registration.js
@@ -4,5 +4,6 @@ exports.post = function (req, res) {
     var name = req.body.name,
         pass1 = req.body.pass,
         pass2 = req.body.passAgain;
+    console.log('name=' + JSON.stringify(req.body));
 
 };
